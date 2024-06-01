08adfa61b52f6fb6a9f7f7455e15c5e9f1f98628
Kush Makkapati <kush.makkapati@icloud.com>
2024-06-01 19:54:55 +0100

    Add hero and features section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        falconred: {
+          100: '#E74C3C',
+          80: '#EB6D60',
+          60: '#F08C82',
+          DEFAULT: '#E74C3C',
+        },
+      },
+      boxShadow: {
+        'hero-image': '0px 0px 25px 4px #E74C3C',
+        'hero-image-hover': '0px 0px 30px 8px #E74C3C',
       },
     },
   },
   plugins: [],
-};
+}
